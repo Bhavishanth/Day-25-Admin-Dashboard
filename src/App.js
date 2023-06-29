0af7773b1da9c069def082d0dc289668adb0ff8c
bhavishanth <bhavishanth@gmail.com>
2023-06-29 13:27:29 +0530

    added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import MainPage from "./components/MainPage";
+import Dashboard from "./components/dashboard/Dashboard";
+import Charts from "./components/Charts";
+import Tables from "./components/Tables";
+import Buttons from "./components/buttons/Buttons";
+import Cards from "./components/buttons/Cards";
+import Color from "./components/utilities/Color";
+import Border from "./components/utilities/Border";
+import Animation from "./components/utilities/Animation";
+import Other from "./components/utilities/Other";
+import LoginPage from "./components/LoginPage";
+import RegisterPage from "./components/RegisterPage";
+import ForgotPassword from "./components/ForgotPassword";
+import Error404 from "./components/Error404";
+import BlankPage from "./components/BlankPage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/forgotpassword" element={<ForgotPassword />} />
+          <Route path="/" element={<MainPage />}>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/charts" element={<Charts />} />
+            <Route path="/tables" element={<Tables />} />
+            <Route path="/components/buttons" element={<Buttons />} />
+            <Route path="/components/cards" element={<Cards />} />
+            <Route path="/utilities/color" element={<Color />} />
+            <Route path="/utilities/border" element={<Border />} />
+            <Route path="/utilities/animation" element={<Animation />} />
+            <Route path="/utilities/other" element={<Other />} />
+            <Route path="/404" element={<Error404 />} />
+            <Route path="/blankpage" element={<BlankPage />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
